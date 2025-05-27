ebd6bc7db6abbb361fa7820c9299e71419df49da
Jakub Solecki <jakubsolecki15@gmail.com>
2025-05-27 15:45:52 +0200

    Riscv final update

--- risc_v_main.asm
+++ risc_v_main.asm
@@ -9,7 +9,7 @@
 	.eqv	FILE_GOTO, 62
 	.eqv	READ_ONLY, 0
 	.eqv	HEADER_SIZE, 54
-	.eqv	MAX_WIDTH, 64
+	.eqv	MAX_WIDTH, 128
 	.eqv	MAX_HEIGHT, 24
 	.eqv	FILEPATH_BUFFSIZE, 100
 	.eqv	WHITE_CHAR, 46 # '.'
@@ -18,20 +18,20 @@
 	# program that prints a maximum of 64 x 24 top left pixels of a 1 bpp BMP file in ascii
 
 	.data
-msg:	.asciz	"Enter path to BMPaa file: "
+msg:	.asciz	"Enter path to BMP file: "
 img:	.space	FILEPATH_BUFFSIZE
-	.align 	2
-spacer:	.space	1		# only here to force alignment of head to middle halfword
-	#.align	1
-	.align 	1
-head:	.space	HEADER_SIZE	
-strd:	.space	MAX_WIDTH
+	.align	2
+spacer:	.space	1		# only here to force alignment of head to middle halfword (no idea why this works)
+	.align	1
+header:	.space	HEADER_SIZE	
+	
+line_to_print:	
+	.space	MAX_WIDTH
+	
 
 	.text
 main:
 	
-
-
 	# print input message
 	li	a7, SYS_PRINT_STR
 	la	a0, msg
@@ -57,9 +57,11 @@ rm_endl_loop:
 
 	
 	# character == '\n'
+	# changes '\n' to 0 at the end of input string
 	addi	t0,t0, -1
 	sb	zero, (t0)
 	
+print_decorating:
 	# new line to separate input and output
 	li	a7, SYS_PRINT_CHAR
 	li	a0, '\n'
@@ -76,18 +78,17 @@ open_file:
 	
 	# jumps to end with code if file didn't open
 	li	s11, -1 # s11 is used only to check if opening a file throws an error
-	beq	a0, s11, fileopenerror
+	beq	a0, s11, file_open_error
 
 read_header:
 	
 	li	a7, FILE_READ
-	la	a1, head
+	la	a1, header
 	li	a2, HEADER_SIZE
 	ecall
 	
 	# reads image width, height, pixel array offset from stored header
-	mv	a0, s1
-	la	t0, head
+	la	t0, header
 	addi	t0, t0, 10	# offset from head to pixel array offset
 	lw	s3, (t0)	# s3 = pixel offset
 	addi	t0, t0, 8	# offset from pixel array offset to bitmap width
@@ -95,83 +96,77 @@ read_header:
 	addi	t0, t0, 4	# offset from bitmap width to bitmap height
 	lw	s10, (t0)	# s10 = image height (in pixels)
 
+calculate_stride:
 
 	# calculates stride size;  stride = (image_width_in_pixels * bits_per_pixel + 31) / 32 * 4
-	# stride - size of single line of pixels (width_in_bytes + )
 	# bits_per_pixel = 1
-	mv	s7, s11	# s7 = image_width_in_pixels (there is no need to multiply * 1 )
-	addi	s7, s7, 31
+	addi	s7, s11, 31	# s7 = image_width_in_pixels + 31 (there is no need to multiply * 1 ) 
 	srli	s7, s7, 5	# s7 = s7 /32
 	slli	s7, s7, 2	# s7 = s7 * 4
 	
 	
-	# number of lines to print
-	# s6 = lines to print
-	li	s6, MAX_HEIGHT
-	ble	s6, s10, get_to_top_left_pixel
-	mv	s6, s10
-	
 get_to_top_left_pixel:
-	li	s9, MAX_WIDTH
-	li	s8, MAX_HEIGHT
-	
-	
-	
+
 	# t2 = adress of current line start
-	mv 	t2, s10		# t0 = img_height 
-	mul	t2, t2, s7 	# t0 *= stride_size
-	add	t2, t2, s3	# t0 += pixel_offset
-	sub	t2, t2, s7 	# t0 -= stride_size
+	mul	t2, s10, s7 	# t2 = img_height * stride_size
+	add	t2, t2, s3	# t2 += pixel_offset
+	sub	t2, t2, s7 	# t2 -= stride_size
 	
 	
-	# moves file handle to t2
+	# moves file handle to t2 position
 	li	a7, FILE_GOTO
 	mv	a1, t2
 	li	a2, 0
 	mv	a0, s1
 	ecall
 	
+set_max_height:
+	# number of lines to print
+	# s6 = lines to print
+	li	s6, MAX_HEIGHT
+	ble	s6, s10, set_max_width
+	mv	s6, s10
 	
-	
-
 set_max_width:
 	# sets t6 to min(width_in_pixels, MAX_WIDTH)
-	mv	t6,  s11 # width in pixels
-	bleu	t6, s9, print_line
-	mv	t6, s9
+	li	t6,  MAX_WIDTH # width in pixels
+	ble	t6, s11, print_line
+	mv	t6, s11
 	
 	
 print_line:
-	mv 	t5, t6  
-	srli	t5, t5, 3 # changes width in pixels to width in bytes
+	srli	t5, t6, 3 # changes width in pixels to width in bytes
 	addi	t5,t5,1
 	# ex. 17 pixels -> 3 bytes, 15 pixels -> 2 bytes
 	
 	
 	# imports stride into memory
-	# strd has the content of given line
-	la	a1, strd
+	# line_to_print has the content of given line
+	la	a1, line_to_print
 	mv	a2, t5
 	li	a7, FILE_READ
 	mv	a0, s1
 	ecall
 	
 	
-	la	s0, strd
+	la	s0, line_to_print
 	addi	s0, s0, -1
 print_setup:
-	# set t4 as msb
+	# set t4 as max value of byte
 	li	t4, 128
 	addi	s0, s0, 1
 	lbu	s2, (s0) 
 
 print_loop:
-	beqz	t6, next_line
-	beqz	t4, print_setup # gets to next byte
-	addi	t6,t6,-1
+	beqz	t6, next_line # if number of pixels to print = 0
+	beqz	t4, print_setup # gets to next byte of pixels
 	and	s4, s2, t4
 	
-	beqz	s4, print_black 
+	addi	t6,t6,-1
+	srli	t4,t4,1
+	
+	
+	beqz	s4, print_black  # if pixel value = 0, print black, else print white
 
 	
 	
@@ -184,17 +179,17 @@ print_black:
 print_chr:
 	li	a7, SYS_PRINT_CHAR
 	ecall
-	srli	t4,t4,1
+	
 	b	print_loop
 	
 next_line:
-	addi	s6, s6, -1
+	addi	s6, s6, -1 # number_of_lines_to_print -= 1
 	li	a7, SYS_PRINT_CHAR
 	li	a0, '\n'
 	ecall
 	
 	# move t2 to lower line
-	sub	t2, t2, s7 # fd = fd - stride
+	sub	t2, t2, s7 # curret_pos = current_pos - stride
 	mv	a0, s1
 	li	a7, FILE_GOTO
 	mv	a1, t2
@@ -202,7 +197,7 @@ next_line:
 	ecall
 	
 	
-	bgtz	s6, set_max_width
+	bgtz	s6, set_max_width # no_of_lines_to_print > 0
 	
 fin:			
 	# close file and exit without error code
@@ -211,7 +206,7 @@ fin:
 	li	a7, SYS_EXIT
 	ecall
 
-fileopenerror:		
+file_open_error:		
 	# error: file didn't open, end with error code -1
 	li	a0, -1
 	li	a7, SYS_EXIT_CODE
